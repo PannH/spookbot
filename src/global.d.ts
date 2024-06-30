a842348ca2371ac633e912d4263642cea1c8897a
PannH <pannh2609@gmail.com>
2024-07-01 01:28:20 +0200

    feat(types): add types for the nickname, picture, and default room name env variables

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,9 @@
 declare namespace NodeJS {
    interface ProcessEnv {
       CLIENT_TOKEN: string;
+      DEFAULT_NICKNAME: string;
+      DEFAULT_PICTURE: string;
+      DEFAULT_ROOM_NAME: string;
+      DATABASE_URL: string;
    }
 }
