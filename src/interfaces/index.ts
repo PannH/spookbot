7166128723bcf511fd581e625c9d853401ce9d26
PannH <pannh2609@gmail.com>
2024-07-01 21:24:33 +0200

    refactor(imports): remove unused import

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -5,7 +5,6 @@ import type Auth from './Auth';
 import type ChatterProfileData from './ChatterProfileData';
 import type Rules from './Rules';
 import type EventOptions from './EventOptions';
-import type CommandOptions from './CommandOptions';
 
 export type {
    CreateRoomOptions,
