a07e4812f7c4eb094a466517ecb5d1dcbcb29ef9
PannH <pannh2609@gmail.com>
2024-07-02 01:27:01 +0200

    feat(commands): add use restrictions and handle them

--- src/events/client/chat.ts
+++ src/events/client/chat.ts
@@ -1,10 +1,11 @@
 import { Event, type Message } from '../../classes';
+import constants from '../../constants';
 
 export default new Event(
    {
       name: 'chat'
    },
-   (client, message: Message) => {
+   async (client, message: Message) => {
       const COMMAND_PREFIXES = ['.', '!', '/'];
 
       if (
@@ -18,7 +19,50 @@ export default new Event(
                command.options.aliases.includes(commandName)
             );
 
-         if (command) command.callback(client, message, args);
+         if (!command) return;
+
+         const { requireAuth, roomOwnerOnly, adminOnly, requiredStaffRoles } =
+            command.options;
+
+         if (requireAuth && !message.chatter.authId)
+            return client.room.sendMessage(
+               'Vous devez être connecté(e) avec Discord ou Twitch pour utiliser cette commande.',
+               'error'
+            );
+
+         const profile = await message.chatter.getProfile();
+
+         if (adminOnly && profile?.staffRole !== 'ADMIN')
+            return client.room.sendMessage(
+               'Vous devez être Administrateur du bot pour utiliser cette commande.',
+               'error'
+            );
+
+         if (
+            requiredStaffRoles &&
+            !requiredStaffRoles.includes(profile?.staffRole) &&
+            profile?.staffRole !== 'ADMIN'
+         ) {
+            const missingRole = requiredStaffRoles.find(
+               (role) => role !== profile?.staffRole
+            );
+
+            return client.room.sendMessage(
+               !profile?.staffRole
+                  ? 'Vous devez faire partie du staff du bot pour utiliser cette commande.'
+                  : `Vous devez être ${constants.STAFF_ROLE_NAMES[missingRole]} du bot pour utiliser cette commande.`,
+               'error'
+            );
+         }
+
+         // TODO: Check if the user is the owner of the room
+         if (roomOwnerOnly && profile?.staffRole !== 'ADMIN')
+            return client.room.sendMessage(
+               'Vous devez être le propriétaire de la salle pour utiliser cette commande.',
+               'error'
+            );
+
+         command.callback(client, message, args);
       }
    }
 );
--- src/interfaces/CommandOptions.ts
+++ src/interfaces/CommandOptions.ts
@@ -1,5 +1,11 @@
+import type { StaffRole } from '@prisma/client';
+
 export default interface CommandOptions {
    name: string;
    description: string;
    aliases?: string[];
+   requireAuth?: boolean;
+   roomOwnerOnly?: boolean;
+   adminOnly?: boolean;
+   requiredStaffRoles?: StaffRole[];
 }
