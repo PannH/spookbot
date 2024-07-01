2a16f377059ec4b73d21b3c80896fbc582ad16a9
PannH <pannh2609@gmail.com>
2024-07-01 20:54:50 +0200

    feat(commands): handle commands

--- src/events/client/chat.ts
+++ src/events/client/chat.ts
@@ -1,10 +1,24 @@
-import { type Chatter, Event } from '../../classes';
+import { Event, type Message } from '../../classes';
 
 export default new Event(
    {
       name: 'chat'
    },
-   (client, chatter: Chatter, content: string) => {
-      console.log(`${chatter.nickname} says "${content}"`);
+   (client, message: Message) => {
+      const COMMAND_PREFIXES = ['.', '!', '/'];
+
+      if (
+         COMMAND_PREFIXES.some((PREFIX) => message.content.startsWith(PREFIX))
+      ) {
+         const args = message.content.split(/ +/gm);
+         const commandName = args.shift().slice(1).toLowerCase();
+         const command =
+            client.commands.get(commandName) ??
+            [...client.commands.values()].find((command) =>
+               command.options.aliases.includes(commandName)
+            );
+
+         if (command) command.callback(client, message, args);
+      }
    }
 );
