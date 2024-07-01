dd206e0f419ff43d5987fc295d4838b9623b7839
PannH <pannh2609@gmail.com>
2024-07-01 21:25:00 +0200

    feat(dictionary): add the dictionary global

--- src/globals.ts
+++ src/globals.ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from '@prisma/client';
+import { Dictionary } from './classes';
 
 const prisma = new PrismaClient();
-
 const baseDir = __dirname.endsWith('src') ? 'src' : 'dist';
+const dictionary = new Dictionary(prisma);
 
-export default { prisma, baseDir };
+export default { prisma, baseDir, dictionary };
