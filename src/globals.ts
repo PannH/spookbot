4303c592910cb366f356d0d623f40b5ab314c8d3
PannH <pannh2609@gmail.com>
2024-07-02 22:47:58 +0200

    feat(globals): create dayjs global

--- src/globals.ts
+++ src/globals.ts
@@ -1,8 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 import { Dictionary } from './classes';
+import initDayjs from 'dayjs';
 
 const prisma = new PrismaClient();
 const baseDir = __dirname.endsWith('src') ? 'src' : 'dist';
 const dictionary = new Dictionary(prisma);
+const dayjs = initDayjs();
 
-export default { prisma, baseDir, dictionary };
+export default { prisma, baseDir, dictionary, dayjs };
