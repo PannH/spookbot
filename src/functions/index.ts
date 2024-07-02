243de446f8813cf0bc083e44838617f48a9966b0
PannH <pannh2609@gmail.com>
2024-07-02 17:45:56 +0200

    feat(functions): create the pluralize function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,5 +2,12 @@ import simplifyString from './simplifyString';
 import shuffleArray from './shuffleArray';
 import determineCategories from './determineCategories';
 import compactNumber from './compactNumber';
+import pluralize from './pluralize';
 
-export { simplifyString, shuffleArray, determineCategories, compactNumber };
+export {
+   simplifyString,
+   shuffleArray,
+   determineCategories,
+   compactNumber,
+   pluralize
+};
