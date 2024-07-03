da8c2cd56ef87483f31231837bac027830cf28b9
PannH <pannh2609@gmail.com>
2024-07-03 19:54:26 +0200

    feat(commands): create the /profile command

---
+++ src/commands/profile.ts
@@ -0,0 +1,69 @@
+import type { Profile, Record } from '@prisma/client';
+import { Command } from '../classes';
+import globals from '../globals';
+import constants from '../constants';
+import { formatStatValue } from '../functions';
+import type { PlayerStats } from '../interfaces';
+
+export default new Command(
+   {
+      name: 'profile',
+      description: "Afficher votre profil ou celui d'un autre joueur.",
+      aliases: ['p']
+   },
+   async (client, message, args) => {
+      const usernameQuery = args[0];
+
+      let profile: Profile & { records: Record[] };
+      if (usernameQuery) {
+         const matchingProfile = await globals.prisma.profile.findFirst({
+            where: {
+               username: usernameQuery
+            },
+            include: {
+               records: true
+            }
+         });
+
+         if (!matchingProfile)
+            return client.room.sendMessage(
+               `Aucun profil trouvé pour la recherche "${usernameQuery}". Veuillez vérifier l'orthographe.`,
+               'error'
+            );
+
+         profile = matchingProfile;
+      } else {
+         if (!message.chatter.authId)
+            return client.room.sendMessage(
+               "Vous n'avez pas de profil. Jouez une partie en étant connecté avec Discord ou Twitch pour le créer automatiquement.",
+               'error'
+            );
+
+         profile = await globals.prisma.profile.findUnique({
+            where: {
+               authId: message.chatter.authId
+            },
+            include: {
+               records: true
+            }
+         });
+
+         if (!profile)
+            return client.room.sendMessage(
+               "Vous n'avez pas de profil. Jouez une partie pour le créer automatiquement.",
+               'error'
+            );
+      }
+
+      const recordsString = profile.records
+         .map(
+            ({ key, value }) =>
+               `${constants.PLAYER_STAT_NAMES[key]} (${formatStatValue(key as keyof PlayerStats, value)})`
+         )
+         .join(' — ');
+
+      client.room.sendMessage(
+         `Profil de ${profile.username}: ${recordsString}`
+      );
+   }
+);
