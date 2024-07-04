1527cfe741a802714ef6873cd2b2433b34244a4f
PannH <pannh2609@gmail.com>
2024-07-04 23:13:48 +0200

    feat(commands): create the /syllables command

--- src/commands/syllables.ts
+++ src/commands/syllables.ts
@@ -1,10 +1,11 @@
 import { Command } from '../classes';
-import { getSyllables } from '../functions';
+import { compactNumber, getSyllables } from '../functions';
+import globals from '../globals';
 
 export default new Command(
    {
       name: 'syllables',
-      description: "Voir toutes les syllabes d'un mot.",
+      description: "Voir toutes les syllabes et leur rareté d'un mot.",
       aliases: ['syl'],
       usage: {
          formats: ['/syl <mot>'],
@@ -18,9 +19,26 @@ export default new Command(
          return client.room.sendMessage('Veuillez indiquer un mot.', 'error');
 
       const syllables = getSyllables(word);
+      const syllablesRarity = syllables.map((syllable) => {
+         const matchingWordsCount =
+            globals.dictionary.searchWords(syllable).length;
+
+         return {
+            syllable,
+            wordsCount: matchingWordsCount
+         };
+      });
+
+      const syllablesString = syllablesRarity
+         .sort((a, b) => a.wordsCount - b.wordsCount)
+         .map(
+            ({ syllable, wordsCount }) =>
+               `${syllable.toUpperCase()} (${compactNumber(wordsCount)})`
+         )
+         .join(', ');
 
       client.room.sendMessage(
-         `Syllabes de ${word.toUpperCase()}: ${syllables.map((syllable) => syllable.toUpperCase()).join(', ')}`
+         `Syllabes de ${word.toUpperCase()}: ${syllablesString}`
       );
    }
 );
