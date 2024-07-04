ffa9b6fc70abc56e056c069bc5266f7635038a7a
PannH <pannh2609@gmail.com>
2024-07-04 19:56:43 +0200

    feat(Record): remove the date column

---
+++ prisma/migrations/20240704175621_mig/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `date` on the `records` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `records` DROP COLUMN `date`;
