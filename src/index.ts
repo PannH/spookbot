3b14c6bc06ef0616d377690b4df283fa1f68de93
PannH <pannh2609@gmail.com>
2024-07-08 21:01:20 +0200

    feat(index): manage startup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,77 @@
-console.log('Hello, world!');
+import globals from './globals';
+import { Client, type Event } from './classes';
+import dotenv from 'dotenv';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import { readdirSync } from 'node:fs';
+
+dotenv.config();
+
+const defaultClient = new Client();
+
+(async () => {
+   const defaultRoom = await globals.prisma.activeRoom.findFirst({
+      where: {
+         isDefault: true
+      },
+      select: {
+         code: true
+      }
+   });
+
+   const rooms = await defaultClient.fetchRooms();
+
+   if (rooms.find((room) => room.roomCode === defaultRoom?.code)) {
+      await defaultClient.joinRoom(defaultRoom.code);
+      defaultClient.room.joinRound();
+   } else {
+      await globals.prisma.activeRoom.deleteMany({
+         where: {
+            isDefault: true
+         }
+      });
+
+      const roomCode = await defaultClient.createRoom({
+         isPublic: false,
+         isDefault: true
+      });
+
+      await defaultClient.joinRoom(roomCode);
+
+      defaultClient.room.joinRound();
+      defaultClient.room.setDefaultRules();
+   }
+
+   await globals.dictionary.initCache();
+})();
+
+(async () => {
+   const httpServer = createServer();
+   const io = new Server(httpServer, {
+      transports: ['websocket']
+   });
+
+   globals.discordSocket = io;
+
+   io.on('connection', (socket) => {
+      globals.logger.info(`New socket connection (${socket.id})`);
+
+      const eventFiles = readdirSync(`${globals.baseDir}/events/discord`);
+
+      for (const file of eventFiles) {
+         const event: Event = require(`./events/discord/${file}`).default;
+         const callbackBind = event.callback.bind(null, defaultClient);
+
+         socket[event.options.isOnce ? 'once' : 'on'](
+            event.options.name,
+            callbackBind
+         );
+      }
+   });
+
+   httpServer.listen(process.env.SERVER_PORT, () =>
+      globals.logger.info(
+         `Socket server listening at http://localhost:${process.env.SERVER_PORT}`
+      )
+   );
+})();
