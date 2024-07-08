b832bde0591155290075cd7807f2d6878976cefa
PannH <pannh2609@gmail.com>
2024-07-08 21:02:47 +0200

    feat(Prisma): handle prisma on new machine startup

--- src/globals.ts
+++ src/globals.ts
@@ -4,15 +4,29 @@ import dayjs from 'dayjs';
 import durationPlugin from 'dayjs/plugin/duration';
 import { Logger } from 'beautify-logs';
 import type { Server } from 'socket.io';
+import { execSync } from 'node:child_process';
+
+const logger = new Logger({
+   format: 1
+});
+
+try {
+   const { PrismaClient } = require('@prisma/client');
+   new PrismaClient();
+} catch (error) {
+   logger.info('Generating Prisma client...');
+   execSync('npm run prisma:generate');
+}
+
+logger.info('Deploying Prisma migrations...');
+
+execSync('npm run prisma:deploy');
 
 dayjs.extend(durationPlugin);
 
 const prisma = new PrismaClient();
 const baseDir = __dirname.endsWith('src') ? 'src' : 'dist';
 const dictionary = new Dictionary(prisma);
-const logger = new Logger({
-   format: 1
-});
 const discordSocket: null | Server = null;
 
 export default { prisma, baseDir, dictionary, dayjs, logger, discordSocket };
