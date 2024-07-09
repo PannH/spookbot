fd70fe245710799b95b033cc035a65a2b46aceb2
PannH <pannh2609@gmail.com>
2024-07-09 17:20:26 +0200

    fix(chatterAdded): prevent the modding of users with no auth

--- src/events/socket/chatterAdded.room.ts
+++ src/events/socket/chatterAdded.room.ts
@@ -13,7 +13,10 @@ export default new Event(
          `Bienvenue ${chatter.nickname} ! Vous pouvez utiliser /help pour découvrir les commandes disponibles et rejoindre le serveur Discord: https://dsc.gg/spookbot`
       );
 
-      if (profile?.staffRole || chatter.authId === client.room.ownerAuthId)
+      if (
+         profile?.staffRole ||
+         (chatter.authId && chatter.authId === client.room.ownerAuthId)
+      )
          chatter.setModerator(true);
    }
 );
