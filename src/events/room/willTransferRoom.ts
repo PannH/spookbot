074d17db8f83b2c9d71da4dc7db94186e4ccfae8
PannH <pannh2609@gmail.com>
2024-08-11 20:25:24 +0200

    fix(willTransferRoom): recreate the default room 10 minutes after room transfer and destroy the other ones

--- src/events/room/willTransferRoom.ts
+++ src/events/room/willTransferRoom.ts
@@ -1,33 +1,46 @@
 import { Client, Event } from '../../classes';
 import { allClients, logger } from '../../globals';
 import { createRoom } from '../../services/api';
-import { createActiveRoom } from '../../services/db';
+import { createActiveRoom, getDefaultActiveRoom } from '../../services/db';
 
 export default new Event('willTransferRoom', async (client) => {
+   const { roomCode } = client.room.data.roomEntry;
+
+   logger.warn(`Room transfer, destroying room: ${roomCode}`);
+
    client.room.destroy();
 
-   const newClient = new Client();
+   const defaultRoom = await getDefaultActiveRoom();
 
-   allClients.push(newClient);
+   if (roomCode !== defaultRoom.code) return;
 
-   const roomCode = await createRoom({
-      creatorUserToken: process.env.CLIENT_USER_TOKEN,
-      gameId: 'bombparty',
-      isPublic: process.env.ENV === 'prod',
-      name: process.env.DEFAULT_ROOM_NAME
-   });
+   setTimeout(
+      async () => {
+         logger.info('Recreating default room 10mins after transfer...');
 
-   await createActiveRoom({
-      code: roomCode,
-      isDefault: true,
-      ownerAuthId: null
-   });
+         const newClient = new Client();
 
-   await newClient.joinRoom(roomCode);
+         allClients.push(newClient);
 
-   logger.info(
-      `Created and joined default room after room transfer: ${roomCode}`
+         const roomCode = await createRoom({
+            creatorUserToken: process.env.CLIENT_USER_TOKEN,
+            gameId: 'bombparty',
+            isPublic: process.env.ENV === 'prod',
+            name: process.env.DEFAULT_ROOM_NAME
+         });
+
+         await createActiveRoom({
+            code: roomCode,
+            isDefault: true,
+            ownerAuthId: null
+         });
+
+         await newClient.joinRoom(roomCode);
+
+         newClient.room.resetRules();
+
+         logger.info(`Default room recreated after transfer: ${roomCode}`);
+      },
+      10 * 60 * 1000
    );
-
-   newClient.room.resetRules();
 });
